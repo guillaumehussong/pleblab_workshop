31d0b258ed8e3fb788b142f3a720f8f0ae3ecc9e
guillaumehussong <planblockchain@gmail.com>
2024-06-12 22:50:02 -0600

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use bdk::{bitcoin::Network, database::MemoryDatabase, Wallet};
 
+fn function_that_takes_a_string(s: &str) {
+    println!("{}", s);
+}
+
 fn main() -> anyhow::Result<()>{
     println!("Hello, {}!", "Pleb");
     dotenv::from_filename(".env").ok();
@@ -8,9 +12,9 @@ fn main() -> anyhow::Result<()>{
     let descriptor = std::env::var("WALLET_DESCRIPTOR")?;
 
     let str_descriptor = "wpkh(tprv8ZgxMBicQKsPd8JL5Q7Z)";
+    let string_desc = String::from(&descriptor);
 
-    println!("Wallet descriptor: {}", descriptor);
-    //dbg!(descriptor);
+    function_that_takes_a_string(str_descriptor);
 
     let wallet = Wallet::new(str_descriptor, None, Network::Testnet, MemoryDatabase::default())?;
 
